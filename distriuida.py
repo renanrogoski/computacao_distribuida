a687e595dc5e9e0d4e8672de32e3cd75ef3ec246
renanrogoski <renanrogoski@gmail.com>
2017-03-29 08:24:10 -0300

    chat funcionando: passar dicionario na url

--- distriuida.py
+++ distriuida.py
@@ -1,10 +1,15 @@
 from bottle import route, run, template
-mensagens = []
+import json
+historico_msgs = []
+
 @route('/hello/<name>')
 def index(name):
-    
-    mensagens.append(name)
-    print mensagens
-    return mensagens
+    msgs = json.loads(name)
+    cliente = msgs['cl']
+    msg = msgs['msg']
+
+    historico_msgs.append({'cliente':cliente,'mensagem':msg})
+
+    return template('<b>Chat {{chat}}</b>!', chat=historico_msgs)
 
 run(host='localhost', port=8080)
