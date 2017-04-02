0036a07bf9a86a0e2a1f27c6e9311a1b2bcb0bcc
renanrogoski <renanrogoski@gmail.com>
2017-04-01 22:13:47 -0300

    chat com get,post e form

--- distriuida.py
+++ distriuida.py
@@ -1,15 +1,41 @@
-from bottle import route, run, template
-import json
-historico_msgs = []
+from bottle import run, get, post, view, request, redirect
+historico_msgs = [("Bem Vindos","Chat Inciado")]
+nome = ""
 
-@route('/hello/<name>')
-def index(name):
-    msgs = json.loads(name)
-    cliente = msgs['cl']
-    msg = msgs['msg']
 
-    historico_msgs.append({'cliente':cliente,'mensagem':msg})
+@get('/mensagens')
+@view('hello_template')
+def hello_template():
+    # msgs = json.loads(name)
+    # cliente = msgs['cl']
+    # msg = msgs['msg']
 
-    return template('<b>Chat {{chat}}</b>!', chat=historico_msgs)
+    # historico_msgs.append({'cliente':cliente,'mensagem':msg})
+
+    # return template('<b>Chat {{chat}}</b>!', chat=historico_msgs)
+    return {'mensagens': historico_msgs, 'nome': nome}
+
+
+
+@post('/mensagens/enviar')
+
+def mensagemEnviar():
+
+    global nome
+
+    msg = request.forms.get('mensagem')
+    n = request.forms.get('nome')
+    historico_msgs.append([n,msg])
+    nome = n
+
+    redirect('/mensagens')
+
+    # msgs = json.loads(name)
+    # cliente = msgs['cl']
+    # msg = msgs['msg']
+
+    # historico_msgs.append({'cliente':cliente,'mensagem':msg})
+
+    # return template('<b>Chat {{chat}}</b>!', chat=historico_msgs)
 
 run(host='localhost', port=8080)
---
+++ hello_template.tpl
@@ -0,0 +1,22 @@
+<html>
+
+<head></head>
+
+<body>
+
+<h1> Chat </h1>
+<ul>
+%for (n, msg) in mensagens:
+    <li> <b>{{n}}: </b> {{msg}} </li>
+%end
+</ul>
+
+<form action="/mensagens/enviar" method=POST>
+    <p> Nome <input name="nome" type="text" value="{{nome}}"/> </p>
+    <p> Mensagem <input name="mensagem" type="text" /> </p>
+    <p> <input value="Enviar" type="submit" /> </p>
+</form>
+
+</body>
+
+</html>
